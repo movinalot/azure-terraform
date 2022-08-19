8ef9eafe4c2ff43885ffa29fa25676fb0ada0537
John McDonough <movinalot@gmail.com>
2022-08-19 10:54:39 -0400

    remove dashes, if any, from SA name

--- user_environment_setup/module_azurerm_storage_account.tf
+++ user_environment_setup/module_azurerm_storage_account.tf
@@ -8,7 +8,7 @@ module "module_azurerm_storage_account" {
 
   resource_group_name      = module.module_azurerm_resource_group[each.value.resource_group_name].resource_group.name
   location                 = module.module_azurerm_resource_group[each.value.resource_group_name].resource_group.location
-  name                     = length(format("%s%s%s", each.value.username, random_id.id[each.value.username].hex, each.value.suffix)) > 24 ? substr(format("%s%s%s", each.value.username, random_id.id[each.value.username].hex, each.value.suffix), 0, 24) : format("%s%s%s", each.value.username, random_id.id[each.value.username].hex, each.value.suffix)
+  name                     = length(replace(format("%s%s%s", each.value.username, random_id.id[each.value.username].hex, each.value.suffix), "-", "")) > 24 ? substr(replace(format("%s%s%s", each.value.username, random_id.id[each.value.username].hex, each.value.suffix), "-", ""), 0, 24) : replace(format("%s%s%s", each.value.username, random_id.id[each.value.username].hex, each.value.suffix), "-", "")
   account_replication_type = local.user_common["storage_account_replication_type"]
   account_tier             = local.user_common["storage_account_tier"]
 }
