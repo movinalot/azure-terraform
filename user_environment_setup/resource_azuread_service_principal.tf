99add1f2e67dce9b1941a3486b2ec66f184a32a6
John McDonough <movinalot@gmail.com>
2022-10-13 01:36:50 -0700

    user credentials csv file

--- user_environment_setup/resource_azuread_service_principal.tf
+++ user_environment_setup/resource_azuread_service_principal.tf
@@ -9,16 +9,3 @@ resource "azuread_service_principal" "service_principal" {
 output "service_principals" {
   value = azuread_service_principal.service_principal
 }
-
-output "service_principals_csv" {
-  value = var.enable_module_output ? [
-    for service_principal in azuread_service_principal.service_principal : 
-    format(
-      "%s,%s,%s,%s",
-      split("-",service_principal.display_name)[0],
-      var.user_password ,service_principal.application_id,
-      azuread_application_password.application_password[split("-",service_principal.display_name)[0]].value
-    )
-  ] : null
-  sensitive = true
-}
---
+++ user_environment_setup/resource_local_sensitive_file.tf
@@ -0,0 +1,29 @@
+locals {
+  user_credentials_tmp = <<-EOT
+%{for user in module.module_azuread_user~}
+%{if local.per_user_service_principle == true}
+${format(
+"\"%s\",\"%s\",\"%s\",\"%s\"",
+user["user"].display_name,
+var.user_password,
+azuread_service_principal.service_principal[user["user"].display_name].application_id,
+azuread_application_password.application_password[user["user"].display_name].value
+)}
+%{endif}
+%{if local.per_user_service_principle == false}
+${format(
+"\"%s\",\"%s\"",
+user["user"].display_name,
+var.user_password
+)}
+%{endif}
+%{endfor~}
+EOT
+
+user_credentials = replace(local.user_credentials_tmp, "/(?m)(?s)(^[\r\n])/", "")
+}
+
+resource "local_sensitive_file" "file" {
+  filename = "${path.module}/user_credentials.csv"
+  content  = local.user_credentials
+}
