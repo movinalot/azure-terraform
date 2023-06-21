b094e1f56034878a217d1bf7aad8fe8b9129b5c5
John McDonough <movinalot@gmail.com>
2023-06-21 13:11:25 -0400

    linted

--- user_environment_setup/scripts/bastion_shareable_link.sh
+++ user_environment_setup/scripts/bastion_shareable_link.sh
@@ -1,41 +1,63 @@
 #!/bin/sh
 
-ARM_CLIENT_ID=${1}
-ARM_CLIENT_SECRET=${2}
-ARM_SUBSCRIPTION_ID=${3}
-ARM_TENANT_ID=${4}
-BASTION_HOST_RG=${5}
-BASTION_HOST=${6}
-BASTION_VM_RG=${7}
-BASTION_VM=${8}
+DEBUG=0 # use 1 for DEBUG output
 
-#eval "$(jq -r '@sh "ARM_CLIENT_ID=\(.armClientId) ARM_CLIENT_SECRET=\(.armClientSecret) ARM_TENANT_ID=\(.armTenantId) ARM_SUBSCRIPTION_ID=\(.armSubscriptionId) BASTION_HOST_RG=\(.bastionHostRg) BASTION_HOST=\(.bastionHost) BASTION_VM_RG=\(.bastionVmRg) BASTION_VM=\(.bastionVm)"')"
+INPUT=../user_credentials.csv
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read -r USERNAME PASSWORD BASTION_HOST_RG BASTION_HOST BASTION_VM ARM_CLIENT_ID ARM_CLIENT_SECRET ARM_SUBSCRIPTION_ID ARM_TENANT_ID
+do
+  # Remove double quotes
+  ARM_CLIENT_ID=${ARM_CLIENT_ID%\"}; ARM_CLIENT_ID=${ARM_CLIENT_ID#\"}
 
-LOGIN=$(az login --service-principal -u ${ARM_CLIENT_ID} -p="${ARM_CLIENT_SECRET}" --tenant ${ARM_TENANT_ID})
+  ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET%\"}; ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET#\"}
 
-BODY=$(cat <<EOF
-{
-  "vms": [
-    { "vm": {
-      "id": "/subscriptions/${ARM_SUBSCRIPTION_ID}/resourceGroups/${BASTION_VM_RG}/providers/Microsoft.Compute/virtualMachines/${BASTION_VM}"
+  ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID%\"}; ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID#\"}
+
+  ARM_TENANT_ID=${ARM_TENANT_ID%\"}; ARM_TENANT_ID=${ARM_TENANT_ID#\"}
+
+  BASTION_HOST_RG=${BASTION_HOST_RG%\"}; BASTION_HOST_RG=${BASTION_HOST_RG#\"}
+
+  BASTION_HOST=${BASTION_HOST%\"}; BASTION_HOST=${BASTION_HOST#\"}
+
+  BASTION_VM_RG=$BASTION_HOST_RG
+
+  BASTION_VM=${BASTION_VM%\"}; BASTION_VM=${BASTION_VM#\"}
+
+  LOGIN=$(az login --service-principal -u "${ARM_CLIENT_ID}" -p="${ARM_CLIENT_SECRET}" --tenant "${ARM_TENANT_ID}")
+
+  if [ "${DEBUG}" -eq 1 ]
+  then
+    echo "${USERNAME}" "${PASSWORD}" "${ARM_CLIENT_ID}" "${ARM_CLIENT_SECRET}" "${ARM_SUBSCRIPTION_ID}" "${ARM_TENANT_ID}" "${BASTION_HOST_RG}" "${BASTION_HOST}" "${BASTION_VM_RG}" "${BASTION_VM}"
+    echo "${LOGIN}"
+  fi
+
+  BODY=$(cat <<EOF
+  {
+    "vms": [
+      { "vm": {
+        "id": "/subscriptions/${ARM_SUBSCRIPTION_ID}/resourceGroups/${BASTION_VM_RG}/providers/Microsoft.Compute/virtualMachines/${BASTION_VM}"
+        }
       }
-    }
-  ]
-}
+    ]
+  }
 EOF
 )
 
-BSL_URI="https://management.azure.com/subscriptions/${ARM_SUBSCRIPTION_ID}/resourceGroups/${BASTION_HOST_RG}/providers/Microsoft.Network/bastionHosts/${BASTION_HOST}"
-API_VER="2022-11-01"
+  BSL_URI="https://management.azure.com/subscriptions/${ARM_SUBSCRIPTION_ID}/resourceGroups/${BASTION_HOST_RG}/providers/Microsoft.Network/bastionHosts/${BASTION_HOST}"
+  API_VER="2022-11-01"
 
-az rest --method post --body "${BODY}" --uri "${BSL_URI}/createShareableLinks?api-version=${API_VER}"
-sleep 10
-BSL_LINK=$(az rest --method post --body "${BODY}" --uri "${BSL_URI}/getShareableLinks?api-version=${API_VER}" | jq -r '.value[].bsl')
-echo $BSL_LINK
+  az rest --method post --headers "Content-Type=application/json"  --body "${BODY}" --uri "${BSL_URI}/createShareableLinks?api-version=${API_VER}"
+  sleep 5
+  BSL_LINK=$(az rest --method post --headers "Content-Type=application/json" --body "${BODY}" --uri "${BSL_URI}/getShareableLinks?api-version=${API_VER}" | jq -r '.value[].bsl')
 
-jq -n \
-  --arg bsl_link ${BSL_LINK} \
-  --arg bsl_user $(echo ${BASTION_HOST_RG} | cut -d'-' -f 1) \
-  '{"bsl":$bsl_link, "usr":$bsl_user}'
+  jq -n \
+    --arg bsl_link "${BSL_LINK}" \
+    --arg bsl_user "$(echo "${BASTION_HOST_RG}" | cut -d'-' -f 1)" \
+    '{ "usr":$bsl_user, "bsl":$bsl_link }'
 
-az logout
+  az logout
+
+done < $INPUT
+IFS=$OLDIFS
