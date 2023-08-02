88bcf213cd6eaf7c53b25a2f9f378066ef471994
John McDonough <movinalot@gmail.com>
2023-08-02 00:42:44 -0400

    copy to public documents

--- user_environment_setup/scripts/winclient_init.ps1
+++ user_environment_setup/scripts/winclient_init.ps1
@@ -13,3 +13,4 @@ netsh advfirewall firewall add rule name="Windows Remote Management (HTTPS-In)" 
 
 Disable-NetAdapterBinding -Name "Ethernet" -ComponentID ms_tcpip6
 Invoke-WebRequest "https://fortinetcloudinttraining.blob.core.windows.net/files-staging/7z2301-x64.exe" -UseBasicParsing -OutFile 7z2301-x64.exe
+Move-Item 7z2301-x64.exe C:/Users/Public/Documents/7z2301-x64.exe
